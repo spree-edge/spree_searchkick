444284ed2c1986720420ec9988999d654f642506
Damian Legawiec <damian@sparksolutions.co>
2020-12-03 14:18:26 +0100

    Fixed migrations for Spree 4.2+

--- db/migrate/20200901123644_add_filters_to_filterable.rb
+++ db/migrate/20200901123644_add_filters_to_filterable.rb
@@ -1,7 +1,7 @@
 class AddFiltersToFilterable < ActiveRecord::Migration[6.0]
   def change
-    add_index :spree_taxonomies, :filterable
-    add_index :spree_properties, :filterable
-    add_index :spree_option_types, :filterable
+    add_index :spree_taxonomies, :filterable unless index_exists?(:spree_taxonomies, :filterable)
+    add_index :spree_properties, :filterable unless index_exists?(:spree_properties, :filterable)
+    add_index :spree_option_types, :filterable unless index_exists?(:spree_option_types, :filterable)
   end
 end
