4f1d363c85001de7648787a9ea15568e2048627d
Bogusław Tolarz <btolarz@gmail.com>
2021-02-09 11:54:05 +0100

    Check filterable presence in option types migration

--- db/migrate/20200714133927_add_filterable_to_spree_option_types.rb
+++ db/migrate/20200714133927_add_filterable_to_spree_option_types.rb
@@ -1,5 +1,10 @@
 class AddFilterableToSpreeOptionTypes < ActiveRecord::Migration[6.0]
   def change
-    add_column :spree_option_types, :filterable, :boolean unless column_exists?(:spree_option_types, :filterable)
+    unless column_exists?(:spree_option_types, :filterable)
+      add_column :spree_option_types, :filterable, :boolean unless column_exists?(:spree_option_types, :filterable)
+    end
+    unless index_exists?(:spree_option_types, :filterable)
+      add_index :spree_option_types, :filterable
+    end
   end
 end
